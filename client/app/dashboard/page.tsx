f469676e563bfd37ddc81cbac031647ce3338152
gabreil <fabrogabrielandres@gmail.com>
2025-11-18 13:49:27 +0100

    Add Page component files for dashboard, expenses, inventory, products, and users with slug handling

---
+++ client/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { use } from "react";
+
+export default function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = use(params);
+  console.log("slug", slug);
+
+  return <>{slug}</>;
+}
---
+++ client/app/products/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { use } from "react"
+
+export default function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = use(params)
+
+  return <>{slug}</>
+}
